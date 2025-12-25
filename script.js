0ecf217c41003cb91df48e85455e667a09289330
muhdsyahirk <muhdsyahirk@gmail.com>
2025-12-26 00:34:41 +0800

    Responsive Nav !!

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
+const navBurger = document.querySelector(".nav-burger");
+const burger_cross = navBurger.querySelector("i");
+const navItem = document.querySelector(".nav-item");
+
+navBurger.addEventListener("click", () => {
+  navItem.classList.toggle("nav-item-mobile");
+  if (burger_cross.classList.contains("fa-bars")) {
+    burger_cross.classList.replace("fa-bars", "fa-times");
+  } else {
+    burger_cross.classList.replace("fa-times", "fa-bars");
+  }
+});
+
+const navLinks = document.querySelectorAll(".nav-item li a");
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    navItem.classList.remove("nav-item-mobile");
+    if (burger_cross.classList.contains("fa-times")) {
+      burger_cross.classList.replace("fa-times", "fa-bars");
+    }
+  });
+});
+
+document.addEventListener("click", (event) => {
+  if (!navBurger.contains(event.target) && !navItem.contains(event.target)) {
+    navItem.classList.remove("nav-item-mobile");
+    if (burger_cross.classList.contains("fa-times")) {
+      burger_cross.classList.replace("fa-times", "fa-bars");
+    }
+  }
+});
+
 // const scroller = document.querySelectorAll(".scroller");
 
 // if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
