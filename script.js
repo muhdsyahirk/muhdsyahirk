965922b7342f8c3b9d6dae770af437ba15fff904
muhdsyahirk <muhdsyahirk@gmail.com>
2025-12-26 01:03:00 +0800

    Fix Responsive Nav

--- script.js
+++ script.js
@@ -4,20 +4,14 @@ const navItem = document.querySelector(".nav-item");
 
 navBurger.addEventListener("click", () => {
   navItem.classList.toggle("nav-item-mobile");
-  if (burger_cross.classList.contains("fa-bars")) {
-    burger_cross.classList.replace("fa-bars", "fa-times");
-  } else {
-    burger_cross.classList.replace("fa-times", "fa-bars");
-  }
+  changeBurgerCross();
 });
 
 const navLinks = document.querySelectorAll(".nav-item li a");
 navLinks.forEach((link) => {
   link.addEventListener("click", () => {
     navItem.classList.remove("nav-item-mobile");
-    if (burger_cross.classList.contains("fa-times")) {
-      burger_cross.classList.replace("fa-times", "fa-bars");
-    }
+    changeBurgerCross();
   });
 });
 
@@ -29,6 +23,14 @@ document.addEventListener("click", (event) => {
     }
   }
 });
+
+function changeBurgerCross() {
+  if (burger_cross.classList.contains("fa-bars")) {
+    burger_cross.classList.replace("fa-bars", "fa-times");
+  } else {
+    burger_cross.classList.replace("fa-times", "fa-bars");
+  }
+}
 
 // const scroller = document.querySelectorAll(".scroller");
 
